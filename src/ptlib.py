56d1dac8ff7bdf0423eae8ff35594d1fde6016e8
PTEC-Repository <65677823+PTEC-Repository@users.noreply.github.com>
2020-05-22 02:13:56 +0100

    Add files via upload

--- src/ptlib.py
+++ src/ptlib.py
@@ -1,6 +1,8 @@
 ver = 'DEVELOPMENT'
 
 import time
+import os
+import subprocess
 
 def currTime():
     return time.strftime('%I:%M:%S%p')
@@ -11,4 +13,31 @@ def currDate():
 def log(txt):
     print(time.strftime('%I:%M:%S%p(%d/%m/%y)=====================') + '\n' + txt + '\n')
     
+def shell(cmd):
+    command = cmd.split(' ')
+    proc = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    out = proc.communicate()[0]
+    out = out.decode('ascii')
+    return out
 
+def command(cmd):
+    os.system(cmd)
+
+def catFile(fName):
+    file = open(fName, 'r')
+    return file.read()
+    file.close()
+
+def checkExists(path):
+    return os.path.exists(path)
+
+def echoToFile(path, txt):
+    file = open(path, 'a+')
+    file.write(txt)
+    file.close()
+
+def overwriteFile(path, txt):
+    file = open(path, 'w')
+    file.write(txt)
+    file.close()
+    
--- src/ptpkglib.py
+++ src/ptpkglib.py
@@ -5,7 +5,8 @@ import os
 import sys
 
 home = os.environ['HOME']
-print('User home is: ' + home)
+mainRepo = 'https://raw.githubusercontent.com/PTEC-Repository/Main-repo/master/'
+repoPath = home + '/PTEC/base/config/repolist'
 
 def checkInstall():
     if os.path.exists(home + '/PTEC/base/'):
@@ -13,15 +14,34 @@ def checkInstall():
     else:
         return False
 
-def Install(pkg):
+if checkInstall():
+    ...
+else:
+    print('Ptpkglib here, PTEC Base is most likley not installed, this will cause SERIOUS complications. Continuing')
 
-    sys.path.append(home + '/PTEC/base/lib')
-    import ptlib
-    import ptpkglib
-    print('Imported PTEC Libries: ptlib')
+def checkRepolist():
+    if len(ptlib.catFile(repoPath).splitlines()) < 1:
+        ptlib.log('Mirror List Is Empty, You Could Run \'ptpkg repolist-gen\'')
+
+def getRepos():
+    try:
+        repos = ptlib.catFile(repoPath).splitlines()
+        return repos
+    except FileNotFoundError:
+        ptlib.log('Repository list not existent, this is most likley due to it not being generated. You could generate it using ptpkg. type \'ptpkg help\' for info on generating the repository list Exiting')
+        exit()
+
+def clearRepo():
+    ptlib.overwriteFile(repoPath, '')
+
+def addRepo(repo):
+    ptlib.echoToFile(repoPath, repo + '\n')
+
+def Install(pkg):
 
     os.system('rm ~/PTEC/base/tmp/* -r')
     print('PTEC Tmp Directory Cleared')
+    repos = getRepos()
 
 def Remove(pkg):
-    print('ptpkglib Remove Function Not Implimented')
+    print('ptpkglib Remove Function Not Implimented (Yet)')
