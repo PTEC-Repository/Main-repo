5ce7881c3dee6f16f0645b5eba22f39ab923b8d2
PTEC-Repository <65677823+PTEC-Repository@users.noreply.github.com>
2020-05-25 18:02:51 +0100

    Add files via upload

--- src/ptlib.py
+++ src/ptlib.py
@@ -1,8 +1,9 @@
-ver = 'DEVELOPMENT'
+ver = '1.0'
 
 import time
 import os
 import subprocess
+import distutils.spawn
 
 def currTime():
     return time.strftime('%I:%M:%S%p')
@@ -28,8 +29,20 @@ def catFile(fName):
     return file.read()
     file.close()
 
-def checkExists(path):
+def exists(path):
     return os.path.exists(path)
+
+def isDir(path):
+    if os.path.isdir(path):
+        return True
+    else:
+        return False
+
+def isFile(path):
+    if os.path.isfile(path):
+        return True
+    else:
+        return False
 
 def echoToFile(path, txt):
     file = open(path, 'a+')
@@ -40,4 +53,51 @@ def overwriteFile(path, txt):
     file = open(path, 'w')
     file.write(txt)
     file.close()
-    
+
+def checkBin(exe):
+    if distutils.spawn.find_executable(exe):
+        return True
+    else:
+        return False
+
+def distro():
+    dist = shell('lsb_release -a').splitlines()[1].split(' ')[1].split('\t')[1]
+    return dist
+
+def arch():
+    return shell('uname -a').split(' ')[12]
+
+def hostname():
+    return shell('uname -a').split(' ')[1]
+
+def distInstallPrompt(pkg):
+    if distro() == 'Ubuntu':
+        return 'sudo apt install ' + pkg
+    elif distro() == 'Arch':
+        return 'sudo pacman -S ' + pkg
+    elif distro() == 'PTECOS':
+        return 'ptpkg i ' + pkg + ' (or sudo pacman -S ' + pkg + ' failing that)'
+     
+def wget(URL, saveLocation):
+    if checkBin('curl'):
+        os.system('curl -s \'' + URL + '\' > ' + saveLocation)
+    else:
+        log('Curl Is Not Installed! It Needs To Be!\n' + distInstallPrompt('curl'))
+
+def unZip(file, location):
+    if isFile(file) and isDir(location):
+        command('unzip \'' + file + '\' -d \'' + location + '\'')
+    else:
+        log('PTLIB: ERROR: unZip(): Input Not File Or Output Not Dir')
+
+
+
+
+
+
+
+
+
+
+
+
--- src/ptpkglib.py
+++ src/ptpkglib.py
@@ -1,4 +1,4 @@
-ver = 'DEVELOPMENT'
+ver = '1.0'
 
 import ptlib
 import os
@@ -7,6 +7,7 @@ import sys
 home = os.environ['HOME']
 mainRepo = 'https://raw.githubusercontent.com/PTEC-Repository/Main-repo/master/'
 repoPath = home + '/PTEC/base/config/repolist'
+ptecPath = home + '/PTEC/'
 
 def checkInstall():
     if os.path.exists(home + '/PTEC/base/'):
@@ -37,11 +38,94 @@ def clearRepo():
 def addRepo(repo):
     ptlib.echoToFile(repoPath, repo + '\n')
 
-def Install(pkg):
+def clearTmp():
+    ptlib.command('rm ' + ptecPath + 'base/tmp/* -r')
 
-    os.system('rm ~/PTEC/base/tmp/* -r')
-    print('PTEC Tmp Directory Cleared')
+def clearCache():
+    ptlib.command('rm ' + ptecPath + 'base/pkg/* -r')
+    print('^ Don\'t Worry About Previous Errors! (If there were any!)\n')
+
+def Download(URL, name):
+    ptlib.wget(URL, ptecPath + '/base/pkg/' + name)
+
+def LInstall(pkg):
+    if ptlib.exists(pkg):
+        clearTmp()
+        ptlib.unZip(pkg, ptecPath + 'base/tmp/')
+        tmp = ptecPath + 'base/tmp/'
+        ver = ptlib.catFile(tmp + 'data/ver')
+        desc = ptlib.catFile(tmp + 'data/desc')
+        name = ptlib.catFile(tmp + 'data/name')
+        category = ptlib.catFile(tmp + 'data/category')
+        ptlib.log('Installing: ' + name + '\n    > Version: ' + ver + '\n    > Description: ' + desc + '\n    > Category: ' + category)
+        name = name.rstrip()
+        print('0% (Copying Assets)')
+        ptlib.command('cp \'' + tmp + name + '\' -r \'' + ptecPath + '\'')
+        print('50% (Copying Binaries)')
+        ptlib.command('cp \'' + tmp + 'exec/' + name +'\' ' + ptecPath + 'base/bin/')
+        print('100% (Done)')
+        ptlib.log('Package: ' + name + ' Installed!')
+        exit()
+    else:
+        ptlib.log('ERROR: Package: \'' + pkg + '\' Not Found!')
+def Install(pkg):
+    pkglist = str()
+    verlist = str()
+    namelist = str()
+    skip = False
+    
+    clearTmp()
+    
     repos = getRepos()
+
+    for repo in repos:
+        pkglist = str()
+        verlist = str()
+        namelist = str()
+        clearTmp()
+        if not skip:
+            ptlib.log('checking repo: ' + repo + 'for package: ' + pkg)
+            ptlib.wget(repo + 'PKGList', ptecPath + 'base/tmp/PKGList')
+            PKGList = ptlib.catFile(ptecPath + 'base/tmp/PKGList')
+            PKGList = PKGList.splitlines()
+            for line in PKGList:
+                pkglist += line.split(' ')[0] + ' '
+                verlist += line.split(' ')[1] + ' '
+                namelist += line.split(' ')[2] + ' '
+            pkglist = pkglist.rstrip().split(' ')
+            verlist = verlist.rstrip().split(' ')
+            namelist = namelist.rstrip().split(' ')
+            if not skip:
+                if pkg in pkglist:
+                    if not skip:
+                        index = pkglist.index(pkg)
+                        ptlib.log('Found: ' + pkg + ' Version: ' + verlist[index] + ' In Repository: ' + repo)
+                        name = namelist[index]
+                        ptlib.log('Downloading: ' + name)
+                        Download(repo + name, name)
+                        ptlib.log('Installing: ' + pkg + '... (' + ptecPath + 'base/pkg' + name + ')')
+                        LInstall(ptecPath + 'base/pkg/' + name)
+                        skip = True
+                        ptlib.log('Skipping All Other Searches...')
+                else:
+                    ptlib.log('Cant Find: ' + pkg + ' In Repository: ' + repo)
+
+def listAll():
+    repos = getRepos()
+    clearTmp()
+    out = str()
+    for repo in repos:
+        clearTmp()
+        ptlib.wget(repo + 'PKGList', ptecPath + 'base/tmp/pkglist')
+        pkglist = ptlib.catFile(ptecPath + 'base/tmp/pkglist').splitlines()
+        out += 'Repo: ' + repo + '\n'
+        for pkg in pkglist:
+            out += '    ' + pkg + '\n'
+    return out
+
+def lCache():
+    out = ptlib.shell('ls ' + ptecPath + 'base/pkg/')
+    ptlib.log('Package Listing Of PTEC Package Cache:\n' + out)
 
 def Remove(pkg):
     print('ptpkglib Remove Function Not Implimented (Yet)')
