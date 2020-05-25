acd30e4da7501aadfd58b2966cff46b39db39a95
PTEC-Repository <65677823+PTEC-Repository@users.noreply.github.com>
2020-05-25 20:29:20 +0100

    Add files via upload

--- setupPTEC.sh
+++ setupPTEC.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo "0%"
 cd ~/
-echo "25%"
-wget https://raw.githubusercontent.com/PTEC-Repository/Main-repo/master/ptecSystem.zip
-echo "33.3%"
-export PATH='$HOME/PTEC/base/bin/:$PATH'
-echo "50%"
-rm ./PTEC/ -r
-echo "75%"
-unzip ptecSystem.zip
-echo "87.5%"
-echo "export $PATH='$HOME/PTEC/base/bin/:$PATH'" >> ~/bash.bashrc
-echo "export $PATH='$HOME/PTEC/base/bin/:$PATH'" >> ~/profile
-echo "100%"
 
-echo "PTPKG SYSTEM INSTALLATION  COMPLETE!"
-echo "Type: 'ptpkg help' For An Introduction To PTPKG!"
+rm ./ptecSystem*
+
+rm ./PTEC -r
+
+wget https://raw.githubusercontent.com/PTEC-Repository/Main-repo/master/ptecSystem.zip
+
+unzip ptecSystem.zip
+
+export PATH='$HOME/PTEC/base/bin/:$PATH'
+
+echo "export $PATH='$HOME/PTEC/base/bin/:$PATH'" >> ~/bash.bashrc
+
+echo "export $PATH='$HOME/PTEC/base/bin/:$PATH'" >> ~/profile
+
+echo "PTPKG Installed, the command ptpkg will now work on any terminal opened from now."
