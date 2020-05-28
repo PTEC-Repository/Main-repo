0bddc31051adf4e0e0e31d170738cfe795dad1fc
PTEC-Repository <65677823+PTEC-Repository@users.noreply.github.com>
2020-05-28 13:18:30 +0100

    Add files via upload

--- src/ptpc-emu/ptlib_ptpc_emu.py
+++ src/ptpc-emu/ptlib_ptpc_emu.py
@@ -6,3 +6,20 @@ def getROM(path):
     with open(path, 'rb') as f:
         out = f.read()
     return out
+
+def getMEM(path):
+    with open(path, 'rb') as f:
+        out = f.read()
+    return out
+
+def writeMEM(index, dec, path):
+    MEM = getMEM(path)
+    MEMobj = open(path, 'w+b')
+    out = list()
+    for i in MEM:
+        out.append(i)
+    MEMobj.write(bytearray(out))
+
+def readMEM(index, path):
+    out = getMEM(path)[index]
+    return out
